d17d1b2b2c01b06e4e859b57548a3535c1e15c64
shell-done <alexandre.thomas@isen-ouest.yncrea.fr>
2020-01-22 21:04:13 +0100

    Setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CarpoolpageComponent } from './carpoolpage/carpoolpage.component';
+import { ProfilepageComponent } from './profilepage/profilepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'mycarpools', component: CarpoolpageComponent },
+  { path: 'myprofile', component: ProfilepageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
